39a8ffdb420146b471dd71267592d1ab2c18e963
John Hawkins <hawkjo@gmail.com>
2026-01-28 18:01:36 +0100

    Updated genome filename

--- examples/count_example.sh
+++ examples/count_example.sh
@@ -4,8 +4,8 @@ scptseq count \
     TX46_Prosalpha3 \
     Prosalpha3 \
     example_data/targets.yml \
-    example_data/Drosophila_melanogaster.BDGP6.28.dna.toplevel.fa \
+    example_data/Drosophila_melanogaster.BDGP6.28.dna.toplevel.chrm_2R.fa \
     example_data/TX46_Prosalpha3_ctrl_bams \
     example_data/TX46_Prosalpha3_bams \
-    --output-dir=count_output \
+    --output-dir=output_count \
     -vvv
